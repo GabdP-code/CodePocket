e8ff8e8b2c48f96cd9638786615fcdb3f192c152
jamesdeperio <deperiojames20@gmail.com>
2018-03-01 09:13:26 +0800

    initial commit

--- app/src/main/java/jdp/codepocket/util/Process.kt
+++ app/src/main/java/jdp/codepocket/util/Process.kt
@@ -1,25 +1,10 @@
 package jdp.codepocket.util
 
-import android.os.AsyncTask
-import android.os.Handler
-import android.os.Looper
-import android.support.v4.app.FragmentActivity
-
 /**
 * Created by jamesdeperio on 01/29/2018.
 **/
 
 object Process {
-    inline fun runOnUIThread(activity: FragmentActivity, crossinline onRunning:() -> Unit) = activity.runOnUiThread { onRunning() }
-    inline fun runOnUIThread(crossinline onRunning:() -> Unit) {
-        Handler(Looper.getMainLooper()).post   { onRunning() }
-    }
-    inline fun runOnBackgroundThread(crossinline onRunning:() -> Unit,crossinline onCompleted:() -> Unit) = AsyncTask.execute {
-        onRunning()
-        onCompleted()
-    }
-    inline fun runOnBackgroundThread(crossinline onRunning:() -> Unit) = AsyncTask.execute { onRunning() }
-
     inline fun tryCatch(onTrying: () -> Unit,onError: (ex:Exception) -> Unit ,onCompleted: () -> Unit) = try { onTrying() }
     catch (e:Exception) { onError(e) }
     finally { onCompleted() }
@@ -29,5 +14,9 @@ object Process {
 
     inline fun tryCatch(onTrying: () -> Unit ) = try { onTrying() }
     catch (e:Exception) { e.printStackTrace() }
+
+    inline fun tryCatch(onTrying: () -> Unit ,onCompleted: () -> Unit ) = try { onTrying() }
+    catch (e:Exception) { e.printStackTrace() }
+    finally {  onCompleted() }
 
 }
